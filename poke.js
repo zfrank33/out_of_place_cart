36ab7626d2c58ac4e6cd85bbf6851cc168e8686e
Francisco Guadarrama <107962016+zfrank33@users.noreply.github.com>
2022-08-16 12:03:58 -0500

    Add files via upload

--- poke.js
+++ poke.js
@@ -1,5 +1,49 @@
 functionaddPokeFiledl
 
+let [...y] = x
+y.push("jjj")
+console.log(x) // will return {name: "strawberry", color: "red"}
+console.log(y)
+function cart(action_string,cart_array,item_obj={'id':""}){
+    cart_array=[[]]
+    let cart_array_len = cart_array.length;
+    
+    switch(action_string) {
+  case "add_to_cart":
+    // code block
+    cart_array_len = cart_array.length;
+    [...cart_array[cart_array_len]] = cart_array[cart_array_len-1]
+    cart_array[cart_array_len].push(1)
+    console.log("Remove all from cart")
+    console.log(cart_array)
+    break;
+  case "remove_from_cart":
+    // code block
+    cart_array_len = cart_array.length;
+    [...cart_array[cart_array_len]] = cart_array[cart_array_len-1]
+    cart_array[cart_array_len].pop()
+    console.log("Remove from cart")
+    console.log(cart_array)
+    break;
+   case "remove_all_from_cart":
+    // code block
+    cart_array_len = cart_array.length;
+    [...cart_array[cart_array_len]] = cart_array[cart_array_len-1]
+    cart_array[cart_array_len].length = 0
+    console.log("Remove all from cart")
+    console.log(cart_array)
+
+    break;
+   case "empty_cart":
+    // code block
+    console.log("Empty cat")
+    break;
+//   default:
+//     pass
+//     // code block
+}
+}
+
 
 
 
--- racer.js
+++ racer.js
@@ -1,104 +1,4 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-    <style>
 
-body {font-family: Arial, Helvetica, sans-serif;}
-* {box-sizing: border-box;}
-
-input[type=text], select, textarea {
-  width: 100%;
-  padding: 12px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  box-sizing: border-box;
-  margin-top: 6px;
-  margin-bottom: 16px;
-  resize: vertical;
-}
-
-
-
-.container {
-  border-radius: 5px;
-  background-color: #f2f2f2;
-  padding: 20px;
-}
-
-
-
-
-        #drivers {
-          font-family: Arial, Helvetica, sans-serif;
-          border-collapse: collapse;
-          width: 100%;
-        }
-        
-        #drivers td, #drivers th {
-          border: 1px solid #ddd;
-          padding: 8px;
-        }
-        
-        #drivers tr:nth-child(even){background-color: #f2f2f2;}
-        
-        #drivers tr:hover {background-color: #ddd;}
-        
-        #drivers th {
-          padding-top: 12px;
-          padding-bottom: 12px;
-          text-align: left;
-          background-color: #04AA6D;
-          color: white;
-        }
-
-
-
-         #getDetailButton {
-  background-color: #04AA6D;
-  color: white;
-  padding: 12px 20px;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-}
-
-  #getDetailButton:hover {
-  background-color: #45a049;
-}
-        </style>
-  </head>
-  <body>
-    <div class="container">
-        <form id="seasonData">
-            <label for="fname">Season</label>
-            <input type="text" id="seasonId" name="season" placeholder="Enter season...">
-        
-            <label for="lname">Round</label>
-            <input type="text" id="roundId" name="round" placeholder="Enter round...">
-            <input type="button" id="submitDet" value="Submit">
-        </form>
-    </div>
-    <h1> Fetched Detail</h1>
-
-<table id="drivers">
-  <tr>
-    <th>First Name</th>
-    <th>Last Name</th>
-    <th>Date of Birth</th>
-    <th>Position</th>
-    <th>Wins</th>
-    <th>Nationality</th>
-    <th>Constructor</th>
-  </tr>
- 
-  
-</table>
-
-    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js">
-    </script>
-    <script>
 // Using JavaScript(axios) and the DOM you are to create a table of data using the F1 racer API. 
 // The table should have a total of 7 columns and dynamically populate the data when a "season" 
 // and "round" are specified within your form.
@@ -150,7 +50,7 @@ for(let i = 0; i<obj.length; i++){
 }
 
 
-}
+
 
 const getDetailButton = document.querySelector('#submitDet');
 getDetailButton.addEventListener('click', function(event){
